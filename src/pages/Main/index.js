f9db2c203adaae1cfc78ab179d19710d6e565835
João Pedro Beck Land <joaopedrobeckland@gmail.com>
2019-06-11 21:12:09 -0300

    update books

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import AuthActions from "../../store/ducks/auth";
+import BookActions from "../../store/ducks/books";
 import { View, Text, TouchableOpacity } from "react-native";
 import SideMenu from "react-native-side-menu";
 import Icon from "react-native-vector-icons/MaterialIcons";
@@ -30,7 +31,7 @@ class Main extends Component {
   };
 
   render() {
-    const { activeList } = this.props;
+    const { activeList, getBooksRequest } = this.props;
     const { leftOpen } = this.state;
 
     return (
@@ -53,7 +54,9 @@ class Main extends Component {
               <Text style={styles.teamTitle}>
                 {activeList ? activeList.name : "Selecione uma lista"}
               </Text>
-              <TouchableOpacity onPress={() => {}} />
+              <TouchableOpacity onPress={() => getBooksRequest()}>
+                <Icon name="refresh" size={24} color="#fff" />
+              </TouchableOpacity>
             </View>
             <Books />
           </View>
@@ -68,7 +71,7 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch =>
-  bindActionCreators(AuthActions, dispatch);
+  bindActionCreators({ ...AuthActions, ...BookActions }, dispatch);
 
 export default connect(
   mapStateToProps,
